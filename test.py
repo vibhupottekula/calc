380df6a4bcb682f3113b6c8c05c7cb81e5f20e14
vibhu <vibhup.btech22@rvu.edu.in>
2023-11-08 18:02:25 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,7 @@
+"""
+This is a simple Python script that adds two numbers.
+"""
+
+A = 1
+B = 2
+print(A + B)
---
+++ test_calculator.py
@@ -0,0 +1,35 @@
+import unittest
+class Calculator:
+    def add(self, x, y):
+        return x + y
+    
+    def subtract(self, x, y):
+        return x - y
+    
+    def multiply(self, x, y):
+        return x * y
+    
+    def divide(self, x, y):
+        if y == 0:
+            raise ValueError("Division by zero is not allowed")
+        return x / y
+
+class CalculatorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_add(self):
+        self.assertEqual(10, self.calc.add(3, 7), "The addition is wrong")
+
+    def test_subtract(self):
+        self.assertEqual(12, self.calc.subtract(15, 3), "Subtraction is wrong")
+
+    def test_multiply(self):
+        self.assertEqual(30, self.calc.multiply(5, 6), "Multiplication is wrong")
+
+    def test_divide(self):
+        self.assertEqual(3, self.calc.divide(6, 2), "Division is wrong")
+
+if __name__ == '__main__':
+    unittest.main()
